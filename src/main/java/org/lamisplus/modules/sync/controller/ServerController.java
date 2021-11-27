4e2aca5e8d9f504f426ce164e18d2114e511504a
Alexander Alozie <alexanderalozie@gmail.com>
2021-11-27 16:02:45 +0100

    Changed object post to the server as byte array instead of string
    Implemented sync history and queue repository and service
    
    Added attribute serverUrl to the ClientController annotated with @Value referenceing lamis.server.url in the application.property file
    
    Added Appoinment table to the Serializer and Deserializer
    
    Commented Test classes to be refactored

--- src/main/java/org/lamisplus/modules/sync/controller/ServerController.java
+++ src/main/java/org/lamisplus/modules/sync/controller/ServerController.java
@@ -1,6 +1,5 @@
 package org.lamisplus.modules.sync.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.lamisplus.modules.sync.service.QueueManager;
@@ -14,19 +13,12 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @RequestMapping("/api/sync")
 public class ServerController {
+
     private final QueueManager queueManager;
 
     @PostMapping("/{table}/{facilityId}")
-    @CircuitBreaker(name = "Sync", fallbackMethod = "getReceiverDefault")
-    public ResponseEntity<String> receiver(
-            @RequestBody String data,
-            @PathVariable String table,
-            @PathVariable Long facilityId) throws IOException {
-        queueManager.queue(data, table, facilityId);
+    public ResponseEntity<String> receiver(@RequestBody byte[] bytes, @PathVariable String table, @PathVariable Long facilityId) throws IOException {
+        queueManager.queue(bytes, table, facilityId);
         return ResponseEntity.ok("Ok");
-    }
-
-    public ResponseEntity<String> getReceiverDefault(Exception e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
