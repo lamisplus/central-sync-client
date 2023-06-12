fa865fc3e91ffb2541c8e908a068dfbc89357957
Mathew77 <mathewadegbite@gmail.com>
2023-06-12 09:58:13 +0100

    adding the backend code

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,5 @@
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
+
 // export const url =  'http://localhost:8787/api/v1/';
 // export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg2NDE5ODA0fQ.ip4Hd_L60e3DP6yzdP-aiovO_QYz9YYifHepiAr_x8OqNn0QhV3MBMpisioTkyvuxY5UGLmKLHRIqa0eO30ZKw'
