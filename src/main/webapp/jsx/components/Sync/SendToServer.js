ac986d9753804cc131dfe67efbea3374ee62ee97
peter.abiodun <asquarep@yahoo.com>
2024-02-15 13:45:17 +0100

    send to server button saving state

--- src/main/webapp/jsx/components/Sync/SendToServer.js
+++ src/main/webapp/jsx/components/Sync/SendToServer.js
@@ -207,16 +207,20 @@ const SendToServer = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (validate()) {
+            setSaving(true);
             var fileUploadErrors = [];
             var errorCount = 0;
             await axios.post(`${baseUrl}sync/sync-history/${getSyncHistoryId()}`, patDetails,
             { headers: { "Authorization": `Bearer ${token}` } })
             .then(response => {
                 alreadyUploaded.current = response.data;
-            })
+            }).catch(error => {
+                setSaving(false);
+                toast.error("could not process request. Please try again.");
+            });
 
             try {
-                setSaving(true);
+                
                 for (let index = 0; index < generatedFiles.length; index++) {
                     const element = generatedFiles[index];
                     if (alreadyUploaded.current.includes(element.fileName)) {
@@ -238,7 +242,11 @@ const SendToServer = (props) => {
                     )
                         .then(response => {
                             setCurrentlyUploading('');
-                            alreadyUploaded.current = [...alreadyUploaded.current, element.fileName];
+                            if (props.isSingleFile === true) {
+                                alreadyUploaded.current = [element.fileName];
+                            } else {
+                                alreadyUploaded.current = [...alreadyUploaded.current, element.fileName];
+                            }
                         })
                         .catch(error => {
                             setCurrentlyUploading('');
@@ -249,7 +257,6 @@ const SendToServer = (props) => {
 
             } catch (error) {
                 console.log(error);
-
             } finally {
                 setSaving(false);
                 if (errorCount > 0) {
@@ -259,7 +266,9 @@ const SendToServer = (props) => {
                     toast.success("Sync Successful. All files uploaded successfully.");
                     props.refreshPrevious();
                 }
-            }};
+            }} else {
+                setSaving(false);
+            };
     }
 
     const getUploadPercentage = () => {
@@ -277,7 +286,7 @@ const SendToServer = (props) => {
 
             <Modal isOpen={props.showModal} toggle={toggleModal} className={props.className} size="lg" backdrop="static">
                 <Form >
-                    <ModalHeader className={classes.header} toggle={props.toggleModal}>SEND TO SERVER </ModalHeader>
+                    <ModalHeader className={classes.header} toggle={toggleModal}>SEND TO SERVER </ModalHeader>
                     <ModalBody>
                         <Typography fontSize={"14px"} marginBottom={"10px"}>
                             {`Sending `} <span style={{ fontWeight: "600" }}>{`${props?.rowObj?.tableName || props?.rowObj?.fileName}`}</span>
@@ -327,7 +336,7 @@ const SendToServer = (props) => {
                                     type='submit'
                                     variant='contained'
                                     disabled={(alreadyUploaded.current.length === generatedFiles.length) || saving}
-                                    style={{ backgroundColor: '#014d88', fontWeight: "bolder" }}
+                                    style={{ backgroundColor: !((alreadyUploaded.current.length === generatedFiles.length) || saving) ?'#014d88' : "#979a9c", fontWeight: "bolder" }}
                                     onClick={handleSubmit}
                                 >
                                     <span style={{ textTransform: "capitalize ", color: "#fff" }}>Send To Server</span>
